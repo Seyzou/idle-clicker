ff4030daf80d3f279f0034dce7d2d95d6eae16bf
Sey <66204578+Seyzou@users.noreply.github.com>
2024-03-25 12:54:51 +0100

    upgrade the code and add a devstats part.

--- script.js
+++ script.js
@@ -1,103 +1,3 @@
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     let data = JSON.parse(localStorage.getItem('idleGameData')) || {
-//         miners: [
-//             { name: "Miner 1", productionPerMinute: 20, baseCost: 100, costMultiplier: 2, owned: 10 },
-//             { name: "Miner 2", productionPerMinute: 2, baseCost: 200, costMultiplier: 2.5, owned: 0 },
-//         ],
-//         money: 0,
-//         productionPerClick: 1,
-//         upgradeCost: 10,
-//         upgradeMultiplier: 2,
-//         minerInterval: 10000, // 1 minute en millisecondes
-//         minerEnabled: false,
-//         minerProduction: 0 // Production totale de tous les mineurs
-//     };
-
-//     let dataStats = JSON.parse(localStorage.getItem('idleGameStats')) || {
-//         playerClic: 0,
-//         playerProduced: 0,
-//         minersProduced: 0,
-//     }
-
-//     const moneyDisplay = document.getElementById('moneyDisplay');
-//     const clickButton = document.getElementById('clickButton');
-//     const buyUpgradeButton = document.getElementById('buyUpgrade');
-//     const buyMinerButton = document.getElementById('buyMiner');
-//     const stats = document.getElementById('stats');
-//     updateStats();
-//     function saveDataToLocal() {
-//         localStorage.setItem('idleGameData', JSON.stringify(data));
-//         localStorage.setItem('idleGameStats', JSON.stringify(dataStats));
-//     }
-
-//     function updateMoneyDisplay() {
-//         moneyDisplay.textContent = `Argent: $${data.money}`;
-//         buyUpgradeButton.textContent = `[${data.productionPerClick}] Production x2 ($${data.upgradeCost})`;
-//         buyMinerButton.textContent = `[${data.miners[0].owned}] ${data.miners[0].name} ($${data.miners[0].baseCost * (data.miners[0].owned + 1) * data.miners[0].costMultiplier})`;
-//     }
-
-//     function updateStats() {
-//         stats.textContent = JSON.stringify(data, null, 2);
-//     }
-
-//     clickButton.addEventListener('click', function () {
-//         data.money += data.productionPerClick;
-//         dataStats.playerClic++;
-//         dataStats.playerProduced = dataStats.playerProduced + data.productionPerClick;
-//         updateMoneyDisplay();
-//         updateStats();
-//         saveDataToLocal();
-//         logEvent('Player clicked to earn money');
-
-//     });
-
-//     buyUpgradeButton.addEventListener('click', function () {
-//         if (data.money >= data.upgradeCost) {
-//             data.money -= data.upgradeCost;
-//             data.productionPerClick += 1;
-//             data.upgradeCost *= data.upgradeMultiplier;
-//             updateMoneyDisplay();
-//             updateStats();
-//             saveDataToLocal();
-//             logEvent('Player bought an upgrade');
-//         }
-//     });
-
-//     buyMinerButton.addEventListener('click', function () {
-//         if (data.money >= data.miners[0].baseCost * (data.miners[0].owned + 1) * data.miners[0].costMultiplier) {
-//             data.money -= data.miners[0].baseCost * (data.miners[0].owned + 1) * data.miners[0].costMultiplier;
-//             data.miners[0].owned += 1;
-//             data.minerEnabled = true;
-//             updateMoneyDisplay();
-//             updateStats();
-//             saveDataToLocal();
-//             logEvent('Player bought a miner');
-
-//         }
-//     });
-
-//     if (data.miners[0].owned + data.miners[1].owned >= 1 || data.minerEnabled) {
-//         setInterval(produceMoney, data.minerInterval); // Commencer la production automatique si c'est le premier mineur acheté
-//         data.minerEnabled = true;
-//     }
-
-//     function produceMoney() {
-//         data.money += (data.miners[0].productionPerMinute * data.miners[0].owned + data.miners[1].productionPerMinute * data.miners[1].owned);
-//         dataStats.minersProduced += (data.miners[0].productionPerMinute * data.miners[0].owned) + (data.miners[1].productionPerMinute * data.miners[1].owned);
-//         updateMoneyDisplay();
-//         updateStats();
-//         saveDataToLocal();
-//         logEvent('Money produced by miners');
-
-
-//     }
-
-
-//     // Charger les données depuis le localStorage lors du chargement de la page
-//     updateMoneyDisplay();
-// });
 function logEvent(event) {
     console.log(`[${new Date().toLocaleString()}] ${event}`);
 }
@@ -121,13 +21,16 @@ document.addEventListener("DOMContentLoaded", function () {
         minerEnabled: false,
 
     };
-
+    let upgradeButtonsVisible = false; // Variable pour suivre l'état d'affichage des boutons
+    let upgradeMinersVisible = false;
     let dataStats = JSON.parse(localStorage.getItem('idleGameStats')) || {
         playerClic: 0,
         playerProduced: 0,
         minersProduced: 0,
     };
     const stats = document.getElementById('stats');
+    const DevStats = document.getElementById('DevStats');
+
     updateStats();
 
     const moneyDisplay = document.getElementById('moneyDisplay');
@@ -144,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function updateStats() {
         stats.textContent = JSON.stringify(dataStats, null, 2);
+        DevStats.textContent = JSON.stringify(data, null, 2);
     }
 
     function saveDataToLocal() {
@@ -154,8 +58,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function createUpgradeButton(upgrade) {
+        const buttonContainer = document.createElement('div');
         const button = document.createElement('button');
         button.textContent = `${upgrade.name} - Cost: $${(upgrade.cost)}`;
+
+        const upgradeInfo = document.createElement('p');
+        upgradeInfo.textContent = `Increase Production by ${upgrade.productionIncrease} Owned: ${upgrade.owned}`;
+        upgradeInfo.style.display = 'block'; // Cachez les informations initialement
+        buttonContainer.appendChild(button);
+        buttonContainer.appendChild(upgradeInfo);
+
         button.addEventListener('click', function () {
             if (data.money >= upgrade.cost) {
                 data.money -= upgrade.cost;
@@ -163,16 +75,26 @@ document.addEventListener("DOMContentLoaded", function () {
                 upgrade.cost = (upgrade.owned ? upgrade.baseCost * (upgrade.costMultiplier * upgrade.owned) : upgrade.baseCost).toFixed(2);
                 data.productionPerClick += upgrade.productionIncrease;
                 saveDataToLocal();
-                button.textContent = `${upgrade.name} - Cost: $${upgrade.cost}`;
+                button.textContent = `${upgrade.name} - Cost: $${(upgrade.cost)}`;
+                upgradeInfo.textContent = `Increase Production by ${upgrade.productionIncrease} \n Owned: ${upgrade.owned}`;
                 logEvent('Player bought a upgrade');
             }
         });
-        return button;
+
+        return buttonContainer;
     }
 
     function createMinerButton(miner) {
+        const buttonContainer = document.createElement('div');
         const button = document.createElement('button');
         button.textContent = `${miner.name} - Cost: $${miner.cost}`;
+
+       const minerInfos = document.createElement('p');
+        minerInfos.textContent = `Produce $${miner.productionPerMinute}/min \n Owned: ${miner.owned}`;
+        minerInfos.style.display = 'block'; // Cachez les informations initialement
+        buttonContainer.appendChild(button);
+        buttonContainer.appendChild(minerInfos);
+
         button.addEventListener('click', function () {
             if (data.money >= miner.cost) {
                 data.money -= miner.cost;
@@ -182,10 +104,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 saveDataToLocal();
                 startMining(); // Démarre la production automatique si nécessaire
                 button.textContent = `${miner.name} - Cost: $${miner.cost}`;
+                minerInfos.textContent = `Produce $${miner.productionPerMinute} \n Owned: ${miner.owned}`;
                 logEvent('Player bought a miner');
             }
         });
-        return button;
+        return buttonContainer;
     }
 
     function updateUpgradeOptions() {
@@ -224,6 +147,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const clickButton = document.getElementById('clickButton');
+    const clickButton2 = document.getElementById('clickButton2');
+
     const buyUpgradeButton = document.getElementById('buyUpgrade');
     const buyMinerButton = document.getElementById('buyMiner');
 
@@ -235,13 +160,38 @@ document.addEventListener("DOMContentLoaded", function () {
         logEvent('Player clicked to earn money');
     });
 
+
+
+    clickButton2.addEventListener('click', function () {
+        data.money += 10000;
+        dataStats.playerClic++;
+        dataStats.playerProduced = dataStats.playerProduced + data.productionPerClick;
+        saveDataToLocal();
+        logEvent('Player clicked to earn money 10000');
+    });
     buyUpgradeButton.addEventListener('click', function () {
-        updateUpgradeOptions();
+        if (!upgradeButtonsVisible) {
+            upgradeOptionsDiv.innerHTML = ''; // Effacez le contenu précédent
+            updateUpgradeOptions()
+            upgradeButtonsVisible = true; // Mettez à jour l'état des boutons comme visibles
+        } else {
+            upgradeOptionsDiv.innerHTML = ''; // Effacez le contenu
+            upgradeButtonsVisible = false; // Mettez à jour l'état des boutons comme masqués
+        }
 
     });
 
     buyMinerButton.addEventListener('click', function () {
-        updateMinerOptions();
+        if (!upgradeMinersVisible) {
+            minerOptionsDiv.innerHTML = ''; // Effacez le contenu précédent
+            updateMinerOptions();
+            upgradeMinersVisible = true; // Mettez à jour l'état des boutons comme visibles
+        } else {
+            minerOptionsDiv.innerHTML = ''; // Effacez le contenu
+            upgradeMinersVisible = false; // Mettez à jour l'état des boutons comme masqués
+        }
+
+
     });
 
     updateStatsDisplay();
